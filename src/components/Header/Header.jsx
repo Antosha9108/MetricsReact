cf89cc064b50a41f4e155ea891080ed1e8719e38
Anton <shelkovnikov9108@gmail.com>
2024-10-05 10:53:23 -0400

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './/Header.css';
+
+export default function Header() {
+    return (
+        <nav>
+            <img href="./src/assets/images/Center Centre Logo.png" alt="center centre logo" />
+            <div>
+                <a href="">Course Topics</a>
+                <a href="">Why UX Outcomes?</a>
+                <a href="">Course Dates</a>
+                <a href="">Sign Up Your Team</a>
+            </div>
+        </nav>
+    );
+}
+
